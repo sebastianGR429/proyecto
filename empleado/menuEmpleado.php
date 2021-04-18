6ea6d5083c9455bd43d380b0c0e57886f33fd93e
Juanbrv00 <juanandresbravoguerrero@hotmail.com>
2021-04-18 04:28:25 -0500

    Vista empleado

---
+++ empleado/menuEmpleado.php
@@ -0,0 +1,49 @@
+	<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.html">
+				<img src="assetsCliente/images/logo/logo2.png" alt="" class="dark-logo">
+				<img src="assetsCliente/images/logo/logo2.png" alt="" class="light-logo">
+			</a>
+			<div class="close-sidebar" data-toggle="left-sidebar-close">
+				<i class="ion-close-round"></i>
+				
+			</div>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-house-1"></span><span href="index.php" class="mtext">Inicio</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="index.php">inicio</a></li>
+						</ul>
+					</li>
+
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-edit2"></span><span href="index.php" class="mtext">Partners</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="index.php">Listar</a></li>
+							<li><a href="index.php">Agregar</a></li>
+					
+						</ul>
+					</li>
+
+                    <li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-apartment"></span><span href="index.php" class="mtext"> Tickets </span>
+						</a>
+						<ul class="submenu">
+                            <li><a href="index.php">Listar </a></li>
+                    	</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+
+	
