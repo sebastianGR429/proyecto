0bcb64d87aea0eb3dc0cee10b6d3e6b3e2384455
Juanbrv00 <juanandresbravoguerrero@hotmail.com>
2021-04-25 02:05:08 -0500

    Empleado modificaciones

--- empleado/perfilEmpleado.php
+++ empleado/perfilEmpleado.php
@@ -1,6 +1,7 @@
 <?php
-include_once($_SERVER['DOCUMENT_ROOT'] . '/proyecto/controlador/ControladorPartner.php');
-include_once($_SERVER['DOCUMENT_ROOT'] . '/proyecto/modelo/daos/DAOPartner.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/proyecto/controlador/ControladorRegistro.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/proyecto/controlador/ControladorEmpleado.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/proyecto/modelo/daos/DAOEmpleado.php');
 session_start();
 if (!isset($_SESSION['user'])) {
     header("location: ../index.php");
@@ -10,8 +11,10 @@ if (!isset($_SESSION['user'])) {
 include('Header.php');
 include('menuEmpleado.php');
 
-$CPartner = new ControladorPartner();
-$partner = $CPartner->listar();
+$conReg=new ControladorRegistro();
+$usuario=$conReg->darUsuario($_SESSION['user']);
+$CEmpleado = new ControladorEmpleado();
+$empleado=$CEmpleado->empleado_x_cod_usuario($usuario->getCod_usuario());
 ?>
 <div class="main-container">
 	<div class="pd-ltr-20 xs-pd-20-10">
@@ -20,12 +23,12 @@ $partner = $CPartner->listar();
 				<div class="row">
 					<div class="col-md-6 col-sm-12">
 						<div class="title">
-							<h4>Gestor de Partner Chibcha-web</h4>
+							<h4>Perfil Empleado Chibcha-web</h4>
 						</div>
 						<nav aria-label="breadcrumb" role="navigation">
 							<ol class="breadcrumb">
-								<li class="breadcrumb-item"><a href="index.html">Partner</a></li>
-								<li class="breadcrumb-item active" aria-current="page">Listar</li>
+								<li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+								<li class="breadcrumb-item active" aria-current="page">Perfil</li>
 							</ol>
 						</nav>
 					</div>
@@ -36,38 +39,23 @@ $partner = $CPartner->listar();
 			<!-- Export Datatable start -->
 			<div class="card-box mb-30">
 				<div class="pd-20">
-					<h4 class="text-blue h4">Partner registradas</h4>
+					<h4 class="text-blue h4">Datos de perfil</h4>
 				</div>
 				<div class="pb-20">
-					<table class="table hover multiple-select-row data-table-export nowrap">
+					<table class="" style="width:100%">
 						<thead>
 							<tr>
-								<th>Nombre partner</th>
+                                <th >Nombre partner</th>
 								<th>Correo</th>
                                 <th>Teléfono</th>
 							</tr>
 						</thead>
-						<?php
-							foreach ($partner as $key) {
-						?>
-							<tbody>
-								<tr> 
-									<td> <?php echo $key['nom_partner']?>  </td> 
-									<td> <?php echo $key['correo_partner'];?>  </td> 
-									<td> <?php echo $key['tel_partner'];?> </td> 
-													
-								</tr>
-							</tbody>
-							<?php
-							}
-							?>
 					</table>
 				</div>
 			</div>
-			<!-- Export Datatable End -->
 		</div>
 	</div>
-</div>
+
 
 
 <script src="TemplateAdministrador/vendors/scripts/core.js"></script>
@@ -95,28 +83,6 @@ $partner = $CPartner->listar();
 <!-- Datatable Setting js -->
 <script src="TemplateAdministrador/vendors/scripts/datatable-setting.js"></script>
 <script>
-
-//Funcion para escalar sugerencias
-function escalarSugerencia() {
-        cod_cliente = $('#cod_cliente').val();
-		desc_escala = $('#desc_escala').val();
-		var dataString = 'cod_cliente=' + cod_cliente + '&desc_escala=' + desc_escala;
-        $.ajax({
-            type: "POST",
-            data: dataString,
-            url: "escalarSugerencia.php",
-
-            success: function(r) {
-                console.log(r);
-                if (r == 1) {
-                    toastr["error"]("Error al subir autor", "Error :(");
-                } else {
-                    toastr["success"]("Sugerencia escalada con exito", "Genial");
-                    // document.getElementById("formSugerencia").reset();
-                }
-            }
-        });
-    }
 
 
 </script>
