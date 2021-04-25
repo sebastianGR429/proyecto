5bf7598fd7bb89f741e3406e227d4b4ca4cbd79c
Juanbrv00 <juanandresbravoguerrero@hotmail.com>
2021-04-25 16:05:54 -0500

    index

--- head.php
+++ head.php
@@ -2,12 +2,13 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Day Bootstrap Template - Index</title>
+  
+  <title>Chibcha Web - Hostings</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <link href="" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
