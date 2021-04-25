4eee2a0c9472fda6abcd4763f13e560599584c04
Juanbrv00 <juanandresbravoguerrero@hotmail.com>
2021-04-25 04:18:44 -0500

    Index

--- footer.php
+++ footer.php
@@ -60,7 +60,7 @@
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span>Day</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Chibcha Web</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
